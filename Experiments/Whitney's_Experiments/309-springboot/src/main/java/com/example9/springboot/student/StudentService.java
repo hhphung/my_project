48b74cd25113920d88d61c5ab182bb82576890c7
Whitney Willoz <wrwilloz@iastate.edu>
2021-09-05 16:00:08 -0500

    made blank app

--- Experiments/Whitney's_Experiments/309-springboot/src/main/java/com/example9/springboot/student/StudentService.java
+++ Experiments/Whitney's_Experiments/309-springboot/src/main/java/com/example9/springboot/student/StudentService.java
@@ -1,2 +1,18 @@
-package com.example9.springboot.student;public class StudentService {
+package com.example9.springboot.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(new Student(1L,"Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY,5),21));
+    }
 }
