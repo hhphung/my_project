60651a1c310e28d57151d752ae3a69c8b6d240d1
hoi phung <hhphung@iastate.edu>
2021-09-08 21:18:41 -0500

    Spring boot

--- Experiments/Hoi/spring boot/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Experiments/Hoi/spring boot/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -14,11 +14,12 @@ import java.util.List;
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-	@GetMapping
-	public int hello() {
-		return "Never gonna give you up\n Never gonna let you down\nNever gonna run around and desert you\n Never gonna make you cry\n Never gonna say goodbye\n Never gonna tell a lie and hurt you";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping
+    public int hello() {
+        return "Never gonna give you up\n Never gonna let you down\nNever gonna run around and desert you\n Never gonna make you cry\n Never gonna say goodbye\n Never gonna tell a lie and hurt you";
+    }
 }
