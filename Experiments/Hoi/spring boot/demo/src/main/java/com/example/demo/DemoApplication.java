593f596092e8a4de4db270d45afdac295dfc30e6
hoi phung <hhphung@iastate.edu>
2021-09-26 17:54:21 -0500

    back end

--- Experiments/Hoi/spring boot/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Experiments/Hoi/spring boot/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,25 +1,23 @@
 package com.example.demo;
 
-
+import com.example.demo.user.User;
+import com.example.demo.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@RestController
+@EnableJpaRepositories
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
-    @GetMapping
-    public int hello() {
-        return "Never gonna give you up\n Never gonna let you down\nNever gonna run around and desert you\n Never gonna make you cry\n Never gonna say goodbye\n Never gonna tell a lie and hurt you";
-    }
+
+
+
 }
