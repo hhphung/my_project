08cdb56b8365542466575ed51d4e123a18f8d588
ARognes <acrognes@iastate.edu>
2021-09-26 01:00:25 -0500

    Added Springboot to backend

---
+++ Backend/src/main/java/coms309/MeetMe/Users/UserRepository.java
@@ -0,0 +1,14 @@
+package coms309.MeetMe.Users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Vivek Bengre
+ *
+ */
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findById(int id);
+    void deleteById(int id);
+}
