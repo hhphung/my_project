89ccd69a1e9febc8db0b4903a9ad7d125403856f
Whitney Willoz <wrwilloz@iastate.edu>
2021-10-07 11:16:18 -0500

    #25 !25 added basic button skeletons

--- Frontend/MeetMe/app/src/main/java/com/example/meetme/LoginPage.java
+++ Frontend/MeetMe/app/src/main/java/com/example/meetme/LoginPage.java
@@ -2,7 +2,11 @@ package com.example.meetme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        Button toRegisterBtn = findViewById(R.id.activity_login_btn_to_register);
+        Button toDashboardBtn = findViewById(R.id.activity_login_btn_to_dash);
+        EditText passwordInput = findViewById(R.id.activity_login_password_input);
+        EditText usernameInput = findViewById(R.id.activity_login_username_input);
+
+        toRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(),RegisterPage.class));
+            }
+        });
+
+        toDashboardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
