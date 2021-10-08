19ae447eebdbe9449edcb469dc6e3ff2ead4aee9
Whitney Willoz <wrwilloz@iastate.edu>
2021-10-08 14:46:05 -0500

    #27 !27 added element skeletons

--- Frontend/MeetMe/app/src/main/java/com/example/meetme/SearchPage.java
+++ Frontend/MeetMe/app/src/main/java/com/example/meetme/SearchPage.java
@@ -1,8 +1,19 @@
 package com.example.meetme;
 
+import static com.example.meetme.api.apiClientFactory.GetUserApi;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.meetme.api.SlimCallback;
+import com.example.meetme.model.User;
 
 public class SearchPage extends AppCompatActivity {
 
@@ -10,5 +21,24 @@ public class SearchPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
+
+        TextView meetingResults = findViewById(R.id.activity_search_meeting_display);
+        EditText meetingInput = findViewById(R.id.activity_search_meeting_name_input);
+        Button backButton = findViewById(R.id.activity_search_btn_to_dash);
+
+
+        meetingResults.setMovementMethod(new ScrollingMovementMethod());
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
     }
+
 }
