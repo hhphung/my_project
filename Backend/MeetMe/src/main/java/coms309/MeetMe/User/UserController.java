b6049edf5db532029e259d8c743eaff38180d4aa
hoi phung <hhphung@iastate.edu>
2021-10-13 21:30:47 -0500

    small add

--- Backend/MeetMe/src/main/java/coms309/MeetMe/User/UserController.java
+++ Backend/MeetMe/src/main/java/coms309/MeetMe/User/UserController.java
@@ -58,7 +58,7 @@ public class UserController {
         return success;
     }
 
-    @GetMapping(path = "/{name}/{password}/login")
+    @GetMapping(path = "/{name}/{password}/login", produces = "application/json")
     String loginUser(@PathVariable String name,@PathVariable String password ) {
         User temp = userRepository.findByName(name);
         if(temp != null) {
