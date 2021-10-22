b80152a07199c70c8c90b6012e4f836e2dd3358f
Whitney Willoz <wrwilloz@iastate.edu>
2021-10-22 16:49:30 -0500

    #39 !39 started over with new idea

--- Frontend/MeetMe/app/src/main/java/com/example/meetme/SearchPage.java
+++ Frontend/MeetMe/app/src/main/java/com/example/meetme/SearchPage.java
@@ -4,6 +4,7 @@ import static com.example.meetme.api.apiClientFactory.GetMeetingApi;
 import static com.example.meetme.api.apiClientFactory.GetUserApi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.DataSetObserver;
@@ -14,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -26,15 +28,16 @@ import com.example.meetme.model.User;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SearchPage extends AppCompatActivity {
+public class SearchPage extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
 
-        ListView meetingResults = findViewById(R.id.activity_search_results);
-        SearchView meetingInput = findViewById(R.id.activity_search_bar);
+        RecyclerView meetingResults = findViewById(R.id.activity_search_results);
+        EditText meetingInput = findViewById(R.id.activity_search_input);
+        Button searchButton = findViewById(R.id.activity_search_btn_to_search);
         Button backButton = findViewById(R.id.activity_search_btn_to_dash);
 
         String username = getIntent().getStringExtra("username");
@@ -48,69 +51,14 @@ public class SearchPage extends AppCompatActivity {
             }
         });
 
-        meetingInput.setInputType(InputType.TYPE_CLASS_TEXT);
-
-        meetingInput.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onQueryTextSubmit(String query) {
-                //do something on text submit
-                ArrayAdapter<String> arr;
-                String err[] = {"No Results", ""};
-                ArrayList<String> tempResults = null;
-
-                if (meetingInput.toString() == "") {
-
-                    GetMeetingApi().getAllMeetings().enqueue(new SlimCallback<List<Meeting>>(sResults -> {
-                        for (Meeting m : sResults) {
-                            String tempString = m.getName();
-                            tempResults.add(tempString);
-                        }
-                    }));
-
-                    String result[] = tempResults.toArray(new String[tempResults.size()]);
-
-                    arr = new ArrayAdapter<String>(getBaseContext(), R.layout.activity_search_page, result);
-
-                } else {
-
-
-                    GetMeetingApi().getResults(meetingInput.toString()).enqueue(new SlimCallback<List<Meeting>>(sResults -> {
-                        for (Meeting m : sResults) {
-                            String tempString = m.getName();
-                            tempResults.add(tempString);
-                        }
-                    }));
-
-                    String result[] = tempResults.toArray(new String[tempResults.size()]);
-
-                    if (result[0] != null) {
-                        arr = new ArrayAdapter<String>(getBaseContext(), R.layout.activity_search_page, result);
-                    } else {
-                        arr = new ArrayAdapter<String>(getBaseContext(), R.layout.activity_search_page, err);
-                    }
-
-                }
-                meetingResults.setAdapter(arr);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-// do nothing when text changes
-                return false;
+            public void onClick(View view) {
+                
             }
         });
 
 
-                meetingResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        Intent intent = new Intent(view.getContext(), CreateMeetingPage.class); //eventually will go to view meeting page
-                        String message = meetingInput.toString();
-                        intent.putExtra("Meeting name", message);
-                        intent.putExtra("username", username);
-                        startActivity(intent);
-                    }
-                });
-            }
-        }
+
+    }
+}
