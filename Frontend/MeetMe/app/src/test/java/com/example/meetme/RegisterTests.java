f9612e0d924197f4681c440c1da57a176de80fcb
Whitney Willoz <wrwilloz@iastate.edu>
2021-10-28 09:57:38 -0500

    #53 !53 changed a gradle property to fix error in CI

--- Frontend/MeetMe/app/src/test/java/com/example/meetme/RegisterTests.java
+++ Frontend/MeetMe/app/src/test/java/com/example/meetme/RegisterTests.java
@@ -23,61 +23,61 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-//@RunWith(RobolectricTestRunner.class)
-//@Config(sdk = Build.VERSION_CODES.O_MR1)
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.O_MR1)
 public class RegisterTests {
-    
 
-//    @Test
-//    public void testPassMisMatch()
-//    {
-//        //Test
-//        RegisterPage activity = Robolectric.buildActivity(RegisterPage.class)
-//                .create()
-//                .resume()
-//                .get();
-//
-//        // enter text
-//        EditText usernameEntry = activity.findViewById(R.id.activity_main_username_input);
-//        usernameEntry.setText("Whittles1");
-//
-//        EditText passwordEntry = activity.findViewById(R.id.activity_main_password_input);
-//        passwordEntry.setText("123");
-//
-//        //purposefully have wrong second password
-//        EditText confirmPassEntry = activity.findViewById(R.id.activity_main_password_input2);
-//        confirmPassEntry.setText("12");
-//
-//        //click create account btn
-//        activity.findViewById(R.id.activity_main_create_account_button).callOnClick();
-//
-//        verify(confirmPassEntry).setError("Passwords do not match. Try again");
-//    }
-//
-//    @Test
-//    public void testPassEmpty()
-//    {
-//        //Test
-//        RegisterPage activity = Robolectric.buildActivity(RegisterPage.class)
-//                .create()
-//                .resume()
-//                .get();
-//
-//        // enter text
-//        EditText usernameEntry = activity.findViewById(R.id.activity_main_username_input);
-//        usernameEntry.setText("Whittles1");
-//
-//        EditText passwordEntry = activity.findViewById(R.id.activity_main_password_input);
-//        passwordEntry.setText("123");
-//
-//        //try to leave confirm blank
-//        EditText confirmPassEntry = activity.findViewById(R.id.activity_main_password_input2);
-//        confirmPassEntry.setText("");
-//        activity.findViewById(R.id.activity_main_create_account_button).callOnClick();
-//
-//        verify(confirmPassEntry).setError("Cannot be empty");
-//
-//    }
+
+    @Test
+    public void testPassMisMatch()
+    {
+        //Test
+        RegisterPage activity = Robolectric.buildActivity(RegisterPage.class)
+                .create()
+                .resume()
+                .get();
+
+        // enter text
+        EditText usernameEntry = activity.findViewById(R.id.activity_main_username_input);
+        usernameEntry.setText("Whittles1");
+
+        EditText passwordEntry = activity.findViewById(R.id.activity_main_password_input);
+        passwordEntry.setText("123");
+
+        //purposefully have wrong second password
+        EditText confirmPassEntry = activity.findViewById(R.id.activity_main_password_input2);
+        confirmPassEntry.setText("12");
+
+        //click create account btn
+        activity.findViewById(R.id.activity_main_create_account_button).callOnClick();
+
+        verify(confirmPassEntry).setError("Passwords do not match. Try again");
+    }
+
+    @Test
+    public void testPassEmpty()
+    {
+        //Test
+        RegisterPage activity = Robolectric.buildActivity(RegisterPage.class)
+                .create()
+                .resume()
+                .get();
+
+        // enter text
+        EditText usernameEntry = activity.findViewById(R.id.activity_main_username_input);
+        usernameEntry.setText("Whittles1");
+
+        EditText passwordEntry = activity.findViewById(R.id.activity_main_password_input);
+        passwordEntry.setText("123");
+
+        //try to leave confirm blank
+        EditText confirmPassEntry = activity.findViewById(R.id.activity_main_password_input2);
+        confirmPassEntry.setText("");
+        activity.findViewById(R.id.activity_main_create_account_button).callOnClick();
+
+        verify(confirmPassEntry).setError("Cannot be empty");
+
+    }
 
 //    @Test
 //    public void testCorrectPass()
