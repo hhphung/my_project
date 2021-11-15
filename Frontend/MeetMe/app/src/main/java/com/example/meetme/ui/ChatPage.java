b9c76bfd72a2882268cb17103e10f80fb7857993
Whitney Willoz <wrwilloz@iastate.edu>
2021-11-15 15:33:54 -0600

    #46 !46 went with different design

--- Frontend/MeetMe/app/src/main/java/com/example/meetme/ui/ChatPage.java
+++ Frontend/MeetMe/app/src/main/java/com/example/meetme/ui/ChatPage.java
@@ -1,7 +1,10 @@
 package com.example.meetme.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -9,8 +12,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.drafts.Draft;
-import org.java_websocket.drafts.Draft_6455;
+
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
@@ -21,9 +23,13 @@ import com.example.meetme.R;
 
 public class ChatPage extends AppCompatActivity {
 
-    Button b1, b2;
-    EditText e1;
-    TextView t1;
+    private WebSocketClient mWebSocketClient;
+
+    private Button bConnect, bDisconnect, bSendButton;
+    private TextView mOutput;
+    private EditText mInput;
+
+    String username = "";
 
 
     @Override
@@ -31,73 +37,98 @@ public class ChatPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_page);
 
-        b1 = (Button) findViewById(R.id.bt1);
-        b2 = (Button) findViewById(R.id.bt2);
-        e1 = (EditText) findViewById(R.id.et2);
-        t1 = (TextView) findViewById(R.id.tx1);
-        String username = getIntent().getStringExtra("username");
+        // Get the buttons
+        bConnect = findViewById(R.id.b_connect);
+        bSendButton = findViewById(R.id.b_sendMessage);
+        bDisconnect = findViewById(R.id.b_Disconnect);
 
-        WebSocketClient cc = null;
+        // Get the textview
+        mOutput = findViewById(R.id.m_output);
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Draft[] drafts = {
-                        new Draft_6455()
-                };
+        // Add scrolling
+        mOutput.setMovementMethod(new ScrollingMovementMethod());
 
-                /**
-                 * If running this on an android device, make sure it is on the same network as your
-                 * computer, and change the ip address to that of your computer.
-                 * If running on the emulator, you can use localhost.
-                 */
-                String w = "ws://10.26.13.93:8080/websocket/" + e1.getText().toString();
-                WebSocketClient cc = null;
+        //Get the editText
+        mInput = findViewById(R.id.m_input);
 
-                try {
-                    Log.d("Socket:", "Trying socket");
-                    cc = new WebSocketClient(new URI(w), (Draft) drafts[0]) {
-                        @Override
-                        public void onMessage(String message) {
-                            Log.d("", "run() returned: " + message);
-                            String s = t1.getText().toString();
-                            t1.setText(s + "\n" + username +": " + message);
-                        }
+        //get username of current client
+        username = getIntent().getStringExtra("username");
 
-                        @Override
-                        public void onOpen(ServerHandshake handshake) {
-                            Log.d("OPEN", "run() returned: " + "is connecting");
-                        }
-
-                        @Override
-                        public void onClose(int code, String reason, boolean remote) {
-                            Log.d("CLOSE", "onClose() returned: " + reason);
-                        }
-
-                        @Override
-                        public void onError(Exception e) {
-                            Log.d("Exception:", e.toString());
-                        }
-                    };
-                } catch (URISyntaxException e) {
-                    Log.d("Exception:", e.getMessage().toString());
-                    e.printStackTrace();
-                }
-                cc.connect();
-
-            }
-        });
-
-        b2.setOnClickListener(new View.OnClickListener() {
+        // Add handlers to the buttons
+        bConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    cc.send(e1.getText().toString());
-                } catch (Exception e) {
-                    Log.d("ExceptionSendMessage:", e.getMessage().toString());
+                connectWebSocket();
+            }
+        });
+
+        bDisconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWebSocketClient.close();
+                finish();
+            }
+        });
+
+        bSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the message from the input
+                String message = mInput.getText().toString();
+
+                // If the message is not empty, send the message
+                if(message != null && message.length() > 0){
+                    mWebSocketClient.send(message);
                 }
             }
         });
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mWebSocketClient.close();
+    }
+
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            /*
+             * To test the clientside without the backend, simply connect to an echo server such as:
+             *  "ws://echo.websocket.org"
+             */
+            uri = new URI("ws://10.0.2.2:8080/example"); // 10.0.2.2 = localhost
+            // uri = new URI("ws://echo.websocket.org");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+            }
+
+            @Override
+            public void onMessage(String msg) {
+                Log.i("Websocket", "Message Received");
+                // Appends the message received to the previous messages
+                mOutput.append("\n" + username + ": " + msg);
+            }
+
+            @Override
+            public void onClose(int errorCode, String reason, boolean remote) {
+                Log.i("Websocket", "Closed " + reason);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
     }
 }
