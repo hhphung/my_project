181004231b94f500272413d74ef64e487dbef37d
Whitney Willoz <wrwilloz@iastate.edu>
2021-11-29 12:42:55 -0600

    #62 !62 added framework for new tests

---
+++ Frontend/MeetMe/app/src/test/java/com/example/meetme/ChatTests.java
@@ -0,0 +1,35 @@
+package com.example.meetme;
+
+import com.example.meetme.ui.RegisterPage;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class ChatTests {
+
+    //private ChatPage activity;
+
+//    @Before
+//    public void setUp() {
+//        //Test
+//        activity = Robolectric.buildActivity(ChatPage.class)
+//                .create()
+//                .resume()
+//                .get();
+//    }
+
+    @Test
+    public void isVisible(){
+
+    }
+
+    @Test
+    public void doesNotSend()
+    {
+        
+    }
+}
